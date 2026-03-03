4d59ae2a14c7f1eab629e67c03e448b63e1c9d20
Cleyton Muto <cleytonmuto@gmail.com>
2026-03-03 13:54:31 -0300

    chore: improved problem 9 implementation

--- 009.py
+++ 009.py
@@ -1,10 +1,10 @@
 def main():
-    for a in range(998):
+    for a in range(1, 998):
         for b in range(a + 1, 999):
-            for c in range(b + 1, 1000):
-                if a ** 2 + b ** 2 == c ** 2 and a + b + c == 1000:
-                    print(a, "*", b, "*", c, "=", a * b * c)
-                    return
+            c = 1000 - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                print(a, "*", b, "*", c, "=", a * b * c)
+                return
     
 
 if __name__ == "__main__":
